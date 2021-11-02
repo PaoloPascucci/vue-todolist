af6eab9e5dba0b20e4f2cdcf5a4a5a81b5ab8ae5
PaoloPascucci <pascuccip9@gmail.com>
2021-11-02 11:55:17 +0100

    Add repo and links, base structure

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = new Vue(
+    {
+        el: 'app',
+        data: {
+            tasks: [
+                'Fare i compiti',
+                'Fare la spesa',
+                'Fare il bucato'
+            ]
+        },
+        methods: {
+
+        }
+    })
