4f7dcb5d16b35c35ce2f1c37a05d1b06b73a3f9c
PaoloPascucci <pascuccip9@gmail.com>
2021-11-02 12:08:20 +0100

    Add Aggiungere task

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = new Vue(
     {
         el: 'app',
+        newTask: '',
         data: {
             tasks: [
                 'Fare i compiti',
@@ -12,6 +13,16 @@ const app = new Vue(
             removeTask(i) {
                 this.tasks.splice(i, 1)
             },
+            addTask() {
+                if (this.newTask != '' && this.newTask.length > 5) {
+                    this.tasks.push(this.newTask)
+                    this.error = false
+                } else {
+                    this.error = true
+                }
+
+                this.newTask = ''
+            }
 
         }
     })
